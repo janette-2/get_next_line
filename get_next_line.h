ce41c0e89ea9bae5f41b86a47060d3556d244d25
Janette Rodriguez Ponce <janerodpon@gmail.com>
2025-10-24 18:26:59 +0200

    Header

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: janrodri <janrodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/24 18:20:14 by janrodri          #+#    #+#             */
+/*   Updated: 2025/10/24 18:26:35 by janrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char    *get_next_line(int fd);
+
+#endif
