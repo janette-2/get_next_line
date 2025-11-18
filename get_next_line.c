8b9fd4b2c743b6fcb8ae096cd1fcd013be63d0a8
Janette Rodriguez Ponce <janerodpon@gmail.com>
2025-11-18 19:42:14 +0100

    18/11/25Commented and completed

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,17 @@
 /*   By: janrodri <janrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 18:31:58 by janrodri          #+#    #+#             */
-/*   Updated: 2025/11/10 21:45:46 by janrodri         ###   ########.fr       */
+/*   Updated: 2025/11/18 19:39:19 by janrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+/* READ_INTO_REMAIN:
+
+Initializes the 'remain' buffer by allocating all the 'read' chars into it. 
+The reading keeps on going until a \n or EOF is found inside of 'remain'.
+*/
 
 char	*read_into_remain(int fd, char *remain)
 {
@@ -37,6 +43,14 @@ char	*read_into_remain(int fd, char *remain)
 	free(buffer);
 	return (remain);
 }
+
+/*EXTRACT_LINE_FROM_REMAIN:
+ 
+When the program calls this function 'remain' has either found a \n inside of it,
+or it has arrived to the EOF. So, either way, we would already have in 'remain'
+the whole line. This function separates the final line, keeps the remain part if 
+there are extra characters, or returns the whole content if EOF is found.
+It will always clean the content of the whole line from the 'remain' buffer. */
 
 char	*extract_line_from_remain(char **remain)
 {
@@ -67,9 +81,16 @@ char	*extract_line_from_remain(char **remain)
 
 /* GET_NEXT_LINE
 
-read_into_remain()-----------------------------------------
-When it starts, it keeps reading untill the storaged chars(remain) 
-contain an '\n' or it finds the EOF. 
+Consist on checking that the 'fd' or BUFFER_SIZE is correct. If so,
+it will retain chars in remain in between calls with the static buffer 'remain'.
+The purpose of 'remain' is to storage the amount of chars specified by 
+'BUFFER_SIZE',(using read_into_remain) in the future calls until a 
+whole line is completed.
+
+When we get 'remain', it will have the line inside, so, to substract this line,
+maintaining the extra chars that the buffer could have, or returning the whole
+line, we get 'line' (using extract_line_from_remain). 
+
 */
 
 char	*get_next_line(int fd)
@@ -96,7 +117,7 @@ char	*get_next_line(int fd)
 	line = extract_line_from_remain(&remain);
 	return (line);
 }
-
+/* 
 #include <fcntl.h>
 #include <stdio.h>
 int	main(void)
@@ -106,8 +127,8 @@ int	main(void)
 	int	count;
 
 	fd = open("Reference_text_THG.txt", O_RDONLY);
-	if (fd < 0)
-		return (1);
+	//--Test with different 'fd' (ex. 0 -> keyboard input)
+	//fd = -1;
 	line = get_next_line(fd);
 	count = 0;
 	while (line && count < 30)
@@ -119,4 +140,9 @@ int	main(void)
 	}
 	close(fd);
 	return (0);
-}
+	//Compile with: 
+	//cc -Wall -Werror -Wextra (...)
+	// get_next_line.c get_next_line_utils.c  -D BUFFER_SIZE=1 && ./a.out 
+
+} 
+*/
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,11 +6,15 @@
 /*   By: janrodri <janrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 14:14:37 by janrodri          #+#    #+#             */
-/*   Updated: 2025/11/10 21:20:04 by janrodri         ###   ########.fr       */
+/*   Updated: 2025/11/18 19:30:32 by janrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+/* FT_STRLEN:
+
+Returns the length of the buffer, without the closing \0 */
 
 int	ft_strlen(char *buffer)
 {
@@ -23,6 +27,10 @@ int	ft_strlen(char *buffer)
 		i++;
 	return (i);
 }
+
+/* FT_STRDUP:
+
+Returns a duplicated buffer of 'src'*/
 
 char	*ft_strdup(char *src)
 {
@@ -41,6 +49,11 @@ char	*ft_strdup(char *src)
 	dup[i] = '\0';
 	return (dup);
 }
+
+/* NEWLINE_INDEX:
+
+Looks for \n in the passed buffer. When found, returns the index of
+it's position (starting at i = 0). If it's not found, returns -1. */
 
 int	newline_index(char *remain)
 {
@@ -63,9 +76,13 @@ len2 bytes from s2 at the end of s1.
 
  Note: ---------------------------------
  ft_strjoin takes ownership of s1 and frees it;
- caller should not use s1 after calling ft_strjoin. 
- This is because malloc and free modify the 'heap' directly.
- So they have a global impact when used in a program.
+ caller should not use s1 after calling ft_strjoin
+ unless the caller assigns s1 to the returned string,
+ transforming s1 to a new string, this execution will
+ delete the content of the old 's1'. 
+ This is because malloc and free work and modify the 
+ 'heap' memory zone directly. They have a global impact,
+ affecting all the functions, when used in a program.
  */
 
 char	*ft_strjoin(char *s1, char *s2, int len2)
@@ -97,10 +114,13 @@ char	*ft_strjoin(char *s1, char *s2, int len2)
 
 /* SUBSTR_AND_FREE
 
-Limits the copied content to the extends of the real string. It does not
-copy further than that, not even if you specify it like so. */
+Creates a new string out of 's', specifying the start index to copy 
+from and the length of the copied content. The function also limits
+the copied content to the extends of the real string.
+It does not copy further than that, not even if you specify
+it like so. */
 
-char *substr_and_free(char *s, int start, int len)
+char	*substr_and_free(char *s, int start, int len)
 {
 	char			*sub;
 	int				i;
@@ -111,8 +131,6 @@ char *substr_and_free(char *s, int start, int len)
 	if (start >= ft_strlen(s))
 		return (NULL);
 	s_len = ft_strlen(s);
-	if (start >= s_len)
-		return (NULL);
 	if (len > s_len - start)
 		len = s_len - start;
 	sub = malloc(len + 1);
@@ -128,4 +146,3 @@ char *substr_and_free(char *s, int start, int len)
 	free(s);
 	return (sub);
 }
-
